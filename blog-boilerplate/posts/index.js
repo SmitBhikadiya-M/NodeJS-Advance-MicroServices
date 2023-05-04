177ed840eaef3ca2dd11c391ef9314b98ea6fbbf
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2023-05-04 10:46:14 +0530

    Update & Added Services For Moderation, Query and Event-Bus

--- blog-boilerplate/posts/index.js
+++ blog-boilerplate/posts/index.js
@@ -1,29 +1,45 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { randomBytes } = require('crypto');
-const cors  = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
-
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    return res.send(posts);
-})
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
-app.post('/posts', (req, res) => {
-    const { title } = req.body;
-    if(!title){
-        return res.status(404).send("Invalid Request");
-    }
-    id = randomBytes(4).toString('hex');
-    posts[id] = { id, title };
-    return res.status(201).send(posts[id]);
-})
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-app.listen(4000, ()=>{
-    console.log('listening on port 4000');
-})
+  posts[id] = {
+    id,
+    title,
+  };
+
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
+  res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
+});
+
+app.listen(4000, () => {
+  console.log("Listening on 4000");
+});
